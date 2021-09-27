08802b1197d3d74dc87800d56b4f1bc14ca75168
Phat Nham <phattannham@gmail.com>
2021-09-27 19:19:21 +0700

    style: update typo

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Bài viết' },
   { href: '/tags', title: 'Chuyên mục' },
   { href: '/projects', title: 'Dự án' },
-  { href: '/about', title: 'Tác giả' },
+  { href: '/about', title: 'Giới thiệu' },
 ]
 
 export default headerNavLinks
