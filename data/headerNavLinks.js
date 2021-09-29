df33a00e329e9b2f42b13bcf637bacd89a8f0bd1
Phat Nham <phattannham@gmail.com>
2021-09-29 16:12:56 +0700

    style: temporarily hide projects and about page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Bài viết' },
   { href: '/tags', title: 'Chuyên mục' },
-  { href: '/projects', title: 'Dự án' },
-  { href: '/about', title: 'Giới thiệu' },
+  // { href: '/projects', title: 'Dự án' },
+  // { href: '/about', title: 'Giới thiệu' },
 ]
 
 export default headerNavLinks
