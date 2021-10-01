108c33acd394981b4826223e560ec70377145e7a
Phat Nham <phattannham@gmail.com>
2021-10-01 10:11:22 +0700

    style: fixed SEO issues

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -62,11 +62,11 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                   </dl>
                   <div className="space-y-3 xl:col-span-3">
                     <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
                         <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
                           {title}
                         </Link>
-                      </h3>
+                      </h2>
                       <div className="flex flex-wrap mt-3 md:mt-1">
                         {tags.map((tag) => (
                           <Tag key={tag} text={tag} />
