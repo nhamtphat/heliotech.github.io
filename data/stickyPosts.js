6cb0e6cb2cbd5b3bfd8c97994f976560b1b1dd23
Phat Nham <phattannham@gmail.com>
2021-10-11 02:39:12 +0700

    post: updated text link

--- data/stickyPosts.js
+++ data/stickyPosts.js
@@ -1,12 +1,12 @@
 const stickyPosts = [
   {
-    title: 'Lộ trình học Laravel',
+    title: 'Lộ trình học Laravel (Laravel Roadmap)',
     description: `Nếu bạn là người mới bắt đầu học Laravel, hãy bắt đầu làm quen từ bài viết này, bài này sẽ cung cấp cho bạn một lộ trình từ Beginner đến Senior.`,
     imgSrc: '/posts/laravel-roadmap/roadmap.png',
     href: '/blog/laravel-roadmap',
   },
   {
-    title: 'Học Laravel căn bản',
+    title: 'Chuỗi bài viết Laravel căn bản',
     description: `Mới làm quen với Laravel, hãy cùng mình bắt đầu hành trình khám phá framework tuyệt vời này nhé, bạn có thể xem lộ trình bên kia, hoặc bắt đầu luôn tại đây.`,
     imgSrc: '/posts/laravel-basic/laravel-basic.png',
     href: '/blog/series/laravel-fundamentals',
